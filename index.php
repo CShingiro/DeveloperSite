5a25e474e7a5c2740e0603e07aaaa694d4065c18
CShingiro <nemyent@gmail.com>
2022-01-27 10:57:42 -0500

    Back to dynamic site.

--- index.php
+++ index.php
@@ -7,18 +7,19 @@
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
   </head>
   <body>
-    <header>
+    <header class="container-fluid">
       <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <div class="container-fluid">
-            <div class="navbar-brand">
-              <img class="logo" src="Images/SRWBLogo.svg">
-            </div>
+            <a class="navbar-brand" href="#">
+              <img class="logo" src="Images/SRWBLogo.png">
+
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav ms mb-2 mb-lg-0">
+              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="parent_navigation nav-item"><a class="navigation nav-link active" aria-current="page" href="#index">Home</a></li>
                 <li class="parent_navigation nav-item"><a class="navigation nav-link active" aria-current="page" href="#about">About</a></li>
                 <li class="parent_navigation nav-item"><a class="navigation nav-link active" aria-current="page" href="#request">Site Request</a></li>
@@ -29,22 +30,30 @@
         </nav>
       </div>
     </header>
-    <main>
-      <div id="index">
-
+    <main class="container-fluid">
+      <div class="container" id="about">
+        <h1>About The Designer</h1>
+          <h2>Projects</h2>
+          <h2>Credentials</h2>
+          <h2>Past Work</h2>
       </div>
-      <div id="about">
-
+      <div class="container" id="request">
+        <h1>Request A Project</h1>
+        <form method="POST"></form>
+        <?php
+        ?>
       </div>
-      <div id="request">
-
-      </div>
-      <div id="contact">
-
+      <div class="container" id="contact">
+        <h1>General Contact Form</h1>
+        <form method="POST"></form>
+        <?php
+        ?>
       </div>
     </main>
-    <footer>
-      <p id="copyright">&#169; - The Socially Radical Guitarist</p>
+    <footer class="container-fluid">
+      <div class="container">
+        <p id="copyright">&#169; - 2022 - Socially Radical Web Design</p>
+      </div>
     </footer>
     <script src="index.js"></script>
     <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js" integrity="sha256-hlKLmzaRlE8SCJC1Kw8zoUbU8BxA+8kR3gseuKfMjxA=" crossorigin="anonymous"></script>

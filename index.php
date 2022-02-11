e863c347814da07a991ca523b7dd910d76139ad6
CShingiro <nemyent@gmail.com>
2022-02-11 02:28:02 -0500

    Test Compact Accordion

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <title>Socially Radical Web Design</title>
   </head>
   <body>
-    <div class="container-fluid">
+    <header>
       <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <div class="container-fluid">
@@ -29,83 +29,94 @@
           </div>
         </nav>
       </div>
-    </div>
-    <div class="container-fluid">
+    </header>
+    <main>
       <div class="container">
-        <div class="accordion" id="accordionExample">
-          <div class="accordion-item" id="about">
-            <h2 class="accordion-header" id="headingOne">
-              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                About The Designer
-              </button>
-            </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <h3>Projects</h3>
-                <h3>Credentials</h3>
-                <h3>Past Work</h3>
+        <div id="about">
+          <h2>About The Designer</h2>
+            <div class="accordion" id="accordionExample">
+              <div id="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                  <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    Projects
+                  </button>
+                </h2>
+              </div>
+              <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  Test #1
+                </div>
+              </div>
+              <div id="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Credentials
+                  </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    Test #2
+                  </div>
+                </div>
+              </div>
+              <div id="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Past Work
+                  </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                  <div class="accordion-body">
+                    Test #3
+                  </div>
+                </div>
               </div>
             </div>
+        </div>
+        <div id="request">
+          <h2>Request A Project</h2>
+            <form>
+              <label for="name">Full Name:</label><br>
+              <input type="text" name="name" placeholder="Full Name" required><br>
+              <label for="email">Email:</label><br>
+              <input type="email" name="email" placeholder="name@example.com" required><br>
+              <h3>Project Type</h3>
+              <input type="radio" id="wordpress" name="project_type" value="Wordpress" checked>
+              <label for="wordpress">Wordpress Site - $500 + $50 per page</label><br>
+              <input type="radio" id="static_site_no_javascript" name="project_type" value="Static - No Javascript">
+              <label for="static_site_no_javascript">Static Site - No Javascript Animations - $700 + $100 per page</label><br>
+              <input type="radio" id="static_site_with_javascript" name="project_type" value="Static + Javascript">
+              <label for="static_site_with_javascript">Static Site With Javascript Animations - $850 + $125 per page</label><br>
+              <input type="radio" id="dynamic_site_no_javascript" name="project_type" value="Dynamic - No Javascript">
+              <label for="dynamic_site_no_javascript">Dynamic Site - No Javascript Animations - $1000 + $150 per page</label><br>
+              <input type="radio" id="dynamic_site_with_javascript" name="project_type" value="Dynamic + Javascript">
+              <label for="dynamic_site_with_javascript">Dynamic Site With Javascript Animations - $1200 +$250 per page</label><br>
+              <input type="submit" formaction="#about" formmethod="POST" value="Order Website">
+            </form>
+            <?php
+              require("PHP/database.php");
+            ?>
           </div>
-          <div class="accordion-item" id="request">
-            <h2 class="accordion-header" id="headingTwo">
-              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Request A Project
-              </button>
-            </h2>
-            <div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <form>
-                  <label for="name">Full Name:</label><br>
-                  <input type="text" name="name" placeholder="Full Name" required><br>
-                  <label for="email">Email:</label><br>
-                  <input type="email" name="email" placeholder="name@example.com" required><br>
-                  <h3>Project Type</h3>
-                  <input type="radio" id="static_site_no_javascript" name="project_type" value="Static - No Javascript" checked>
-                  <label for="static_site_no_javascript">Static Site - No Javascript Animations - $700 + $100 per page</label><br>
-                  <input type="radio" id="static_site_with_javascript" name="project_type" value="Static + Javascript">
-                  <label for="static_site_with_javascript">Static Site With Javascript Animations - $850 + $125 per page</label><br>
-                  <input type="radio" id="dynamic_site_no_javascript" name="project_type" value="Dynamic - No Javascript">
-                  <label for="dynamic_site_no_javascript">Dynamic Site - No Javascript Animations - $1000 + $150 per page</label><br>
-                  <input type="radio" id="dynamic_site_with_javascript" name="project_type" value="Dynamic + Javascript">
-                  <label for="dynamic_site_with_javascript">Dynamic Site With Javascript Animations - $1200 +$250 per page</label><br>
-                  <input type="submit" formaction="#about" formmethod="POST" value="Order Website">
-                </form>
-                <?php
-                  require("PHP/database.php");
-                ?>
-              </div>
-            </div>
-          </div>
-          <div class="accordion-item" id="contact">
-            <h2 class="accordion-header" id="headingThree">
-              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                General Contact Form
-              </button>
-            </h2>
-            <div id="collapseThree" class="accordion-collapse collapse show" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-              <div class="accordion-body">
-                <form method="POST"></form>
-                <label for="name">Full Name:</label><br>
-                <input type="text" name="name" placeholder="Full Name" required><br>
-                <label for="email">Email:</label><br>
-                <input type="email" name="email" placeholder="name@example.com" required><br>
-                <label for="message">Message:</label><br>
-                <input type="text" name="message" placeholder="Message" required><br>
-                <?php
-                  require("PHP/database.php");
-                ?>
-              </div>
-            </div>
+          <div id="contact">
+            <h2>General Contact Form</h2>
+              <form method="POST"></form>
+              <label for="name">Full Name:</label><br>
+              <input type="text" name="name" placeholder="Full Name" required><br>
+              <label for="email">Email:</label><br>
+              <input type="email" name="email" placeholder="name@example.com" required><br>
+              <label for="message">Message:</label><br>
+              <input type="text" name="message" placeholder="Message" required><br>
+              <?php
+                require("PHP/database.php");
+              ?>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="container-fluid">
+      </main>
+    <footer>
       <div class="container">
         <p id="copyright">&#169; - 2022 - Socially Radical Web Design</p>
       </div>
-    </div>
+    </footer>
     <script src="index.js"></script>
     <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js" integrity="sha256-hlKLmzaRlE8SCJC1Kw8zoUbU8BxA+8kR3gseuKfMjxA=" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>

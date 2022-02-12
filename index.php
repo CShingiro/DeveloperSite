1e2102f3ae64b68ae550623a60eccc2e62c09cfb
CShingiro <nemyent@gmail.com>
2022-02-12 18:08:19 -0500

    Patch to Credentials #2

--- index.php
+++ index.php
@@ -65,11 +65,11 @@
                 <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                   <div class="accordion-body">
                     <h3>Awards</h3><br>
-                    <iframe src="Images/AwardLetter.pdf" width="100%" height="auto"></iframe><br>
+                    <iframe src="Images/AwardLetter.pdf" width="100%" height="500px"></iframe><br>
                     <br>
                     <h3>Education</h3><br>
-                    <iframe src="Images/ConestogaOfficialTranscript.pdf" width="100%" height="auto"></iframe><br><br>
-                    <iframe src="Images/ProofofProgramCompletion.pdf" width="100%" height="auto"></iframe><br>
+                    <iframe src="Images/ConestogaOfficialTranscript.pdf" width="100%" height="500px"></iframe><br><br>
+                    <iframe src="Images/ProofofProgramCompletion.pdf" width="100%" height="500px"></iframe><br>
                     <br>
                     <h3>Certificates</h3><br>
                     <p>Coming Soon! After convocation!</p>

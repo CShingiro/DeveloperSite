d0f98294ef7b7c38d6650195d87d8872680358ea
CShingiro <nemyent@gmail.com>
2022-02-12 16:05:09 -0500

    Pilot Carousel and Biography

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <div class="container-fluid">
             <a class="navbar-brand" href="#">
-              <img class="logo" src="Images/SRWBLogo.png" class="d-inline-block align-text-top">
-              Socially Radical Web Design at a Socially Reasonable Price
+              <img class="logo" src="Images/SRWBLogo.png" class="d-inline-block align-text-top" alt="Socially Radical Web Design Logo">
+              <p class="main-site-font">Socially Radical Web Design at a Socially Reasonable Price</p>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
@@ -35,20 +35,74 @@
     <br>
     <br>
     <main>
+      <div id="carouselCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+          <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+          <button type="button" data-bs-target="#carouselCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="Images/Showcase1.png" class="d-block w-100" alt="First Showcase Site">
+            <h5 class="main-site-font">Wordpress Individual Project</h5>
+            <p><a href="https://dev-shingiroportfolio.pantheonsite.io/">Link to Site Here</p>
+          </div>
+          <div class="carousel-item">
+            <img src="Images/Showcase2.png" class="d-block w-100" alt="Second Showcase Site">
+            <h5 class="main-site-font">Socially Radical Guitarist Pilot Blog</h5>
+            <p><a href="https://live-guitarenthusiasm.pantheonsite.io/">Link to Site Here</p>
+          </div>
+          <div class="carousel-item">
+            <img src="Images/Showcase3.png" class="d-block w-100" alt="Third Showcase Site">
+            <h5 class="main-site-font">Socially Radical Guitarist Pilot Blog #2</h5>
+            <p><a href="https://guitarenthusiasm.wordpress.com/">Link to Site Here</p>
+          </div>
+          <div class="carousel-item">
+            <img src="Images/Showcase4.png" class="d-block w-100" alt="Fourth Showcase Site">
+            <h5 class="main-site-font">Collaborative Wordpress Project</h5>
+            <p><a href="https://dev-chrisolysimhotel.pantheonsite.io/">Link to Site Here</p>
+          </div>
+          <div class="carousel-item">
+            <img src="Images/Showcase5.png" class="d-block w-100" alt="Fifth Showcase Site">
+            <h5 class="main-site-font">Collaborative Coffee Shop Project</h5>
+            <p><a href="https://cshingiro.github.io/Team6Project/">Link to Site Here</p>
+          </div>
+          <div class="carousel-item">
+            <img src="Images/Showcase6.png" class="d-block w-100" alt="Sixth Showcase Site">
+            <h5 class="main-site-font">The Socially Radical Guitarist Website</h5>
+            <p><a href="https://sociallyradicalguitarist.com">Link to Site Here</p>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselCaptions" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselCaptions" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
 
       <div class="container">
         <div id="about">
-          <h2>About The Designer</h2>
+          <h2 class="main-site-font">About The Designer</h2>
             <div class="accordion" id="accordionExample">
               <div id="accordion-item">
                 <h2 class="accordion-header" id="headingOne">
                   <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                    Projects
+                    Biography
                   </button>
                 </h2>
                 <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                   <div class="accordion-body">
-                    Test #1
+                    <p><strong>Christian Shingiro</strong> is a recent graduate of the Internet Applications and Web Development Fundamentals Program at Conestoga College.
+                      He already has a few projects under his belt, and is the web master and host of a radio show called <a href="https://sociallyradicalguitarist.com">
+                      The Socially Radical Guitarist</a>. His specialties are Wordpress, Bootstrap, PHP, MySQL, and general Full Stack web development.
+                      He aims to be Socially Radical, while providing web services at a rate that's socially reasonable.
+                    </p>
                   </div>
                 </div>
               </div>
@@ -60,14 +114,14 @@
                 </h2>
                 <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                   <div class="accordion-body">
-                    <h3>Awards</h3><br>
+                    <h3 class="main-site-font">Awards</h3><br>
                     <iframe src="Images/AwardLetter.pdf" width="60%" height="300px"></iframe><br>
                     <br>
-                    <h3>Education</h3><br>
+                    <h3 class="main-site-font">Education</h3><br>
                     <iframe src="Images/ConestogaOfficialTranscript.pdf" width="60%" height="300px"></iframe><br><br>
-                    <iframe src="Images/ProofofProgramCompletion.pdf" width="60%" height="300px"></iframe><br>                    
+                    <iframe src="Images/ProofofProgramCompletion.pdf" width="60%" height="300px"></iframe><br>
                     <br>
-                    <h3>Certificates</h3><br>
+                    <h3 class="main-site-font">Certificates</h3><br>
                     <p>Coming Soon! After convocation!</p>
                   </div>
                 </div>
@@ -92,14 +146,14 @@
         <br>
         <div class="container">
           <div id="request">
-            <h2>Request A Project</h2>
+            <h2 class="main-site-font">Request A Project</h2>
               <form>
                 <label for="name">Full Name:</label><br>
                 <input type="text" name="name" placeholder="Full Name" required><br>
                 <label for="email">Email:</label><br>
                 <input type="email" name="email" placeholder="name@example.com" required><br>
                 <br>
-                <h3>Project Type</h3>
+                <h3 class="main-site-font">Project Type</h3>
                 <input type="radio" id="wordpress" name="project_type" value="Wordpress" checked>
                 <label for="wordpress">Wordpress Site - $500 + $50 per page</label><br>
                 <input type="radio" id="static_site_no_javascript" name="project_type" value="Static - No Javascript">
@@ -119,7 +173,7 @@
           <br>
           <div class="container">
             <div id="contact">
-              <h2>General Contact Form</h2>
+              <h2 class="main-site-font">General Contact Form</h2>
                 <form method="POST"></form>
                 <label for="name">Full Name:</label><br>
                 <input type="text" name="name" placeholder="Full Name" required><br>

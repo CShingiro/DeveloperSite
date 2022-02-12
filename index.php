6e803d5d1b345d91d949d636c98befe8a258f10c
CShingiro <nemyent@gmail.com>
2022-02-12 17:50:39 -0500

    Switch places test

--- index.php
+++ index.php
@@ -201,8 +201,10 @@
     <br>
     <br>
     <footer>
-      <p id="copyright">&#169; - 2022 - Socially Radical Web Design</p>
-      <a href="https://linkedin.com/in/sociallyradicalwebdesign.com"><img src="Images/linkedin.svg"></a>
+      <div class="container">
+        <a href="https://linkedin.com/in/sociallyradicalwebdesign.com"><img src="Images/linkedin.svg"></a>
+        <p id="copyright">&#169; - 2022 - Socially Radical Web Design</p>
+      </div>
     </footer>
     <script src="index.js"></script>
     <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js" integrity="sha256-hlKLmzaRlE8SCJC1Kw8zoUbU8BxA+8kR3gseuKfMjxA=" crossorigin="anonymous"></script>

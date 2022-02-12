cb483f61a22a5bc51a2f711f5c778f15207c7ba5
CShingiro <nemyent@gmail.com>
2022-02-12 17:20:04 -0500

    Removing Slogan Patch

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
           <div class="container-fluid">
             <a class="navbar-brand" href="#">
               <img class="logo" src="Images/SRWBLogo.png" class="d-inline-block align-text-top" alt="Socially Radical Web Design Logo">
-              Socially Radical Web Design at a Socially Reasonable Price
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>

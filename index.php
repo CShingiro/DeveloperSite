8c60bbdf30ef9fc3f23e7277ca8f65b1099661e6
CShingiro <nemyent@gmail.com>
2022-02-13 00:20:37 -0500

    Favicon add trial

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="icon" href="Images/SRWBLogo.svg" width="30" height="30" type="image/png">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
     <title>Socially Radical Web Design</title>
   </head>

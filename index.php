73b49221ebdacb02ede2e74d75aa720fadafc4ec
CShingiro <nemyent@gmail.com>
2022-02-13 01:09:10 -0500

    Cleanup for Efficiency

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="icon" href="Images/SRWBLogo.png" width="30" height="30" type="image/png">
     <link rel="stylesheet" href="webdesign.css" type="text/css">
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
     <title>Socially Radical Web Design</title>
   </head>
   <body>
@@ -36,7 +35,7 @@
     <br>
     <br>
     <main>
-      <p class="text-center fst-italic">Socially Radical Web Design at a Socially Reasonable Price</p><br><br>
+      <p id="slogan" class="text-center">Socially Radical Web Design at a Socially Reasonable Price</p><br><br>
       <div class="container bg-success bg-gradient bg-opacity-50 rounded-3 border border-dark border-5">
         <div id="about">
           <h2 class="text-center">About The Designer</h2>
@@ -219,18 +218,13 @@
     <br>
     <footer>
       <div class="container">
-        <a href="https://linkedin.com/in/sociallyradicalwebdesign.com"><img src="Images/linkedin.svg" width="60px" height="60px"></a>
-        <a href="#"><img src="Images/facebook.svg" width="60px" height="60px"></a>
-        <a href="#"><img src="Images/instagram.svg" width="60px" height="60px"></a>
-        <a href="#"><img src="Images/twitter.svg" width="60px" height="60px"></a>
+        <a href="https://linkedin.com/in/sociallyradicalwebdesign.com"><img src="Images/linkedin.svg" width="60px" height="60px" alt="LinkedIn Icon"></a>
+        <a href="#"><img src="Images/facebook.svg" width="60px" height="60px" alt="Facebook Icon"></a>
+        <a href="#"><img src="Images/instagram.svg" width="60px" height="60px" alt="Instagram Icon"></a>
+        <a href="#"><img src="Images/twitter.svg" width="60px" height="60px" alt="Twitter Icon"></a>
         <p id="copyright">&#169; - 2022 - Socially Radical Web Design</p>
       </div>
     </footer>
-    <script src="index.js"></script>
-    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js" integrity="sha256-hlKLmzaRlE8SCJC1Kw8zoUbU8BxA+8kR3gseuKfMjxA=" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/react@17/umd/react.production.min.js" crossorigin></script>
-    <script src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js" crossorigin></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>

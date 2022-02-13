864869e3b2bedb20a58cd4c7958921363e34bbf1
CShingiro <nemyent@gmail.com>
2022-02-13 01:34:59 -0500

    Adding Currency Notice

--- index.php
+++ index.php
@@ -160,7 +160,8 @@
           <div id="request">
             <h2>Request A Project</h2>
             <span class="text-danger">**Note: Please allow 4-6 weeks for delivery.**</span><br>
-            <span class="text-danger">**Fees include cost of all required hosting services, including URL for one year, and MySQL space, if requested.**</span><br><br>
+            <span class="text-danger">**Fees include cost of all required hosting services, including URL for one year, and MySQL space, if requested.**</span><br>
+            <span class="text-danger">**Prices are all in Canadian Dollars (CAD).**</span><br><br>
             <form>
               <h3>Personal Information</h3><br>
               <label for="name">Full Name:</label><br>

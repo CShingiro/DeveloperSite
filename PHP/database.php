5035f2794f0af07b681b262d21bcb62268778310
CShingiro <nemyent@gmail.com>
2022-02-14 14:02:43 -0500

    Fix to Database charset

--- PHP/database.php
+++ PHP/database.php
@@ -11,5 +11,4 @@
 
     $db_connect = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
         OR die('Could not connect to MySQL: '. mysqli_connect_error());
-    mysqli_set_charset($db, 'utf8');
 ?>

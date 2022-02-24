c910356105de129002d733b80730e4a634eb8d01
CShingiro <nemyent@gmail.com>
2022-02-24 02:58:13 -0500

    More Concise Button patch

--- frindex.php
+++ frindex.php
@@ -221,7 +221,7 @@
               <input type="text" id="subject" name="subject" placeholder="Subject" required><br>
               <label for="message">Message:</label><br>
               <textarea style="height: 300px" class="form-control" id="message" placeholder="Message/Comment" name="message" required></textarea><br>
-              <input type="submit" formaction="#contact" formmethod="POST" name="contact" value="Contacter Le Conception Des Sites Web Socialement Radicale">
+              <input type="submit" formaction="#contact" formmethod="POST" name="contact" value="Contacter Le Conception Web Socialement Radicale">
             </form><br>
             <?php
               if (isset($_POST['contact'])) {

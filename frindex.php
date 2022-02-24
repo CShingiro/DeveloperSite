fc8d392d4a40f618b816f7254cc10b7d6a8c5892
CShingiro <nemyent@gmail.com>
2022-02-24 03:02:42 -0500

    Title Patch

--- frindex.php
+++ frindex.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="icon" href="Images/SRWBLogo.png" width="30" height="30" type="image/png">
     <link rel="stylesheet" href="webdesign.css" type="text/css">
-    <title>Conception Web Socialement Radical</title>
+    <title>Conception Web Socialement Radicale</title>
   </head>
   <body>
     <header>
